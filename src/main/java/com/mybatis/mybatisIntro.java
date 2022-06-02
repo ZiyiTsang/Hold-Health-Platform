68267773c2504e5eee509660f815f803a338a7d7
ZIYI ZENG <CME1909120@XMU.EDU.MY>
2022-06-02 20:28:58 +0800

    mybatis test

--- src/main/java/com/mybatis/mybatisIntro.java
+++ src/main/java/com/mybatis/mybatisIntro.java
@@ -1,7 +1,23 @@
 package com.mybatis;
 
-public class mybatisIntro {
-    public static void main(String[] args) {
+import com.POJO.articleBean;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class mybatisIntro {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlsession=sqlSessionFactory.openSession();
+        List<articleBean> articles=sqlsession.selectList("article.selectAll");
+        System.out.print(articles);
+        sqlsession.close();
     }
 }
