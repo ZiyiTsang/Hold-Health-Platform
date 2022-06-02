7e04f818e3106cbe2c059d5622198b4a9de725a8
ZIYI ZENG <CME1909120@XMU.EDU.MY>
2022-06-02 15:34:49 +0800

    add maven framwork

--- src/main/java/com/JDBC/articleDAO.java
+++ src/main/java/com/JDBC/articleDAO.java
@@ -4,29 +4,4 @@ public class articleDAO {
 
 }
 
-class article{
-    public String getTitle() {
-        return title;
-    }
-    public String getContent() {
-        return content;
-    }
-    public String getID() {
-        return ID;
-    }
-    public void setID(String ID) {
-        this.ID = ID;
-    }
-    public void setPublish_time(String publish_time) {
-        this.publish_time = publish_time;
-    }
-    public String getPublish_time() {
-        return publish_time;
-    }
 
-    private String ID;
-    private String publish_time;
-    public String title;
-    public String content;
-
-}
